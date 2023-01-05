30021965bd2bd30101039e22cf88a40e09aa0f1c
iampr <iampr@PRABIN>
2023-01-05 23:04:48 +0545

    Add project files.

---
+++ 13(loops)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your target ");
+        int UserTarget = int.Parse(Console.ReadLine());
+        int start = 0;
+        while(start <= UserTarget)
+        {
+            Console.WriteLine(start);
+            start += 2; 
+        }
+    }
+}
